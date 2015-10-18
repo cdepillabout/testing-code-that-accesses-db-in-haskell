05fd8b659a8449f4b7925e8922dc3562d1eb1651
(cdep)illabout <cdep.illabout@gmail.com>
2015-10-18 09:18:32 +0900

    Add some comments.

--- without-db/free-monad/src/Lib.hs
+++ without-db/free-monad/src/Lib.hs
@@ -1,3 +1,6 @@
+-- Inspired by
+-- https://hbtvl.wordpress.com/2015/06/28/servant-persistent-and-dsls.
+
 {-# LANGUAGE ConstraintKinds            #-}
 {-# LANGUAGE DataKinds                  #-}
 {-# LANGUAGE DeriveGeneric              #-}
@@ -47,13 +50,26 @@ import Servant
     ( (:<|>)(..), (:>), Capture, Delete, FromText(..), Get, JSON, Post, Put
     , ReqBody, ServantErr(..), Server, err404, serve )
 
--- Inspired by
--- https://hbtvl.wordpress.com/2015/06/28/servant-persistent-and-dsls.
-
 ----------------------------------
 -- Persistent model definitions --
 ----------------------------------
 
+-- This uses Persistent (a database library) to define a BlogPost data
+-- type as well as it's corresponding database table.
+--
+-- If you've never used Persistent, you can think of it as defining the
+-- following data types and sql statement. This is a vast simplification of
+-- what it is actually doing, but it's good for a start:
+--
+-- data BlogPost = BlogPost { blogPostTitle :: Text
+--                          , blogPostContent :: Text
+--                          }
+--
+-- type Key BlogPost = Int
+--
+-- CREATE TABLE "blog_post" ("id" PRIMARY KEY,"title" VARCHAR,"content" VARCHAR)
+--
+-- It also defines some helper functions to query the "blog_post" table.
 share [ mkPersist sqlSettings, mkMigrate "migrateAll"]
       [persistLowerCase|
 BlogPost json
@@ -62,11 +78,29 @@ BlogPost json
     deriving Show
 |]
 
+--------------------------------
+-- DSL for accessing database --
+--------------------------------
 
--------------------------
--- DSL for persistent? --
--------------------------
-
+-- | The whole point of this "free-monad" example is the couple of lines
+-- below.  We are defining a DSL that represents actions that can be
+-- performed on a database.
+--
+-- A 'DbAction' is the /type/ of an action that can be performed on
+-- a database.  For example, 'GetDb' is a data constructor that represents
+-- getting a row of data from the database.  'InsertDb' is a data
+-- constructor that represents putting a row of data into the database.
+--
+-- 'DbDSL' represents a sequence of 'DbAction's.  The magic starts to come
+-- in later on in this file.  We will construct an /interpreter/ that takes
+-- a 'DbDSL' and actually performs the actions against a real database.
+-- Then, in testing, we will write a /different interpreter/ that just
+-- operates on a hashmap in memory.  It never actually operates on a real
+-- database.
+--
+-- PROTIP: The following is not actually a free-monad, but is instead using
+-- the operational monad.  In practice, it's not a huge difference, but it
+-- is something to be aware of.
 type DbDSL = Program DbAction
 data DbAction a where
     ThrowDb  :: ServantErr -> DbAction a
@@ -75,27 +109,32 @@ data DbAction a where
     UpdateDb :: Key BlogPost -> BlogPost -> DbAction ()
     DelDb    :: Key BlogPost -> DbAction ()
 
--- | throws an error
+-- The following helper functions make it easy to create a 'DbDSL'.  The
+-- most interesting one is 'getOr404Db', which combines both 'getDb' and
+-- 'throwDb' into one 'DbDSL'.
+
+-- | Throws an error.
 throwDb :: ServantErr -> DbDSL a
 throwDb err = singleton (ThrowDb err)
 
--- | dual of `persistent`'s `get`
+-- | Get a 'BlogPost' from the database.
 getDb :: Key BlogPost -> DbDSL (Maybe BlogPost)
 getDb key = singleton (GetDb key)
 
--- | dual of `persistent`'s `insert`
+-- | Insert a 'BlogPost' into the database and return its id.
 insertDb :: BlogPost ->  DbDSL (Key BlogPost)
 insertDb blogPost = singleton (InsertDb blogPost)
 
--- | dual of `persistent`'s `update`
+-- | Update a 'BlogPost' that already exists in the database.
 updateDb :: Key BlogPost -> BlogPost -> DbDSL ()
 updateDb key blogPost = singleton (UpdateDb key blogPost)
 
--- | dual of `persistent`'s `delete`
+-- | Delete a 'BlogPost' from the database.
 deleteDb :: Key BlogPost -> DbDSL ()
 deleteDb key = singleton (DelDb key)
 
--- | like `getDb` but throws a 404 if it could not find the corresponding record
+-- | Try to get a 'BlogPost' from the database.  If it doesn't exist, throw
+-- an error.
 getOr404Db :: Key BlogPost -> DbDSL BlogPost
 getOr404Db key = do
     maybeVal <- getDb key
@@ -103,6 +142,67 @@ getOr404Db key = do
         Just blogPost -> return blogPost
         Nothing -> throwDb err404
 
+-----------------
+-- servant api --
+-----------------
+
+-- | This defines a type which represents the API.  A description of the
+-- API is given in the README.md.  If you read the README.md, this should
+-- be very understandable.
+type BlogPostApi = "create" :> ReqBody '[JSON] BlogPost
+                            :> Post '[JSON] (Key BlogPost)
+
+              :<|> "read"   :> Capture "id" (Key BlogPost)
+                            :> Get '[JSON] BlogPost
+
+              :<|> "update" :> Capture "id" (Key BlogPost)
+                            :> ReqBody '[JSON] BlogPost
+                            :> Put '[JSON] ()
+
+              :<|> "delete" :> Capture "id" (Key BlogPost)
+                            :> Delete '[JSON] ()
+
+-- | This defines handlers for our API.  This 'server' function is
+-- Servant-specfic and not too interesting.  If you want to learn more
+-- about it, see the Servant tutorial.
+--
+-- The interesting thing is the 'createBlogPost', 'readBlogPost',
+-- 'updateBlogPost', and 'deleteBlogPost' functions.  See their
+-- documentation for an explanation of what they are doing.
+server :: (forall a . DbDSL a -> EitherT ServantErr IO a) -> Server BlogPostApi
+server interpreter = createBlogPost
+                :<|> readBlogPost
+                :<|> updateBlogPost
+                :<|> deleteBlogPost
+  where
+    -- This is the handler for the API call that creates a blog post.
+    --
+    -- Looking at the type, you can see that we get a 'BlogPost' object as
+    -- input, and we need to return a 'Key' 'BlogPost' (which you can think
+    -- of as an integer that corresponds to a database id).
+    --
+    --
+    createBlogPost :: BlogPost -> EitherT ServantErr IO (Key BlogPost)
+    createBlogPost blogPost = interpreter $ insertDb blogPost
+
+    readBlogPost :: Key BlogPost -> EitherT ServantErr IO BlogPost
+    readBlogPost key = interpreter $ getOr404Db key
+
+    updateBlogPost :: Key BlogPost -> BlogPost -> EitherT ServantErr IO ()
+    updateBlogPost key val = interpreter $ updateDb key val
+
+    deleteBlogPost :: Key BlogPost -> EitherT ServantErr IO ()
+    deleteBlogPost key = interpreter $ deleteDb key
+
+blogPostApiProxy :: Proxy BlogPostApi
+blogPostApiProxy = Proxy
+
+
+
+
+------------------------------
+-- database dsl interpreter --
+------------------------------
 
 runDbDSLInServant :: SqlBackend
                   -> DbDSL a
@@ -135,46 +235,6 @@ runDbDSLInServant conn dbDSL =
                 -- conn <- ask
                 -- liftIO $ connRollback conn (getStmtConn conn)
                 throwM servantErr
-
------------------
--- servant api --
------------------
-
-type BlogPostApi = "create" :> ReqBody '[JSON] BlogPost
-                            :> Post '[JSON] (Key BlogPost)
-
-              :<|> "read"   :> Capture "id" (Key BlogPost)
-                            :> Get '[JSON] BlogPost
-
-              :<|> "update" :> Capture "id" (Key BlogPost)
-                            :> ReqBody '[JSON] BlogPost
-                            :> Put '[JSON] ()
-
-              :<|> "delete" :> Capture "id" (Key BlogPost)
-                            :> Delete '[JSON] ()
--------------------------------------------
-
--- Servant HTTP handlers for BlogPost CRUD requests.
-server :: (forall a . DbDSL a -> EitherT ServantErr IO a) -> Server BlogPostApi
-server interpreter = createBlogPost
-                :<|> readBlogPost
-                :<|> updateBlogPost
-                :<|> deleteBlogPost
-  where
-    createBlogPost :: BlogPost -> EitherT ServantErr IO (Key BlogPost)
-    createBlogPost blogPost = interpreter $ insertDb blogPost
-
-    readBlogPost :: Key BlogPost -> EitherT ServantErr IO BlogPost
-    readBlogPost key = interpreter $ getOr404Db key
-
-    updateBlogPost :: Key BlogPost -> BlogPost -> EitherT ServantErr IO ()
-    updateBlogPost key val = interpreter $ updateDb key val
-
-    deleteBlogPost :: Key BlogPost -> EitherT ServantErr IO ()
-    deleteBlogPost key = interpreter $ deleteDb key
-
-blogPostApiProxy :: Proxy BlogPostApi
-blogPostApiProxy = Proxy
 
 defaultMain :: IO ()
 defaultMain =
